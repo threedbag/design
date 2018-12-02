e2a35fbd822b81c9c1d0e525760a562e2f476c7a
threedbag <389124628@qq.com>
2018-12-02 21:13:47 +0800

    二叉树的最大深度

---
+++ leecode/src/main/java/com/baka/code/MaximumDepthofBinaryTree.java
@@ -0,0 +1,57 @@
+package com.baka.code;
+//く__,.ヘヽ.　　　　/　,ー､ 〉
+//        　　　　　＼ ', !-─‐-i　/　/´
+//        　　　 　 ／｀ｰ'　　　 L/／｀ヽ､
+//        　　 　 /　 ／,　 /|　 ,　 ,　　　 ',     ？？？？？？？？
+//        　　　ｲ 　/ /-‐/　ｉ　L_ ﾊ ヽ!　 i       ？BUG哪来的呢？
+//        　　　 ﾚ ﾍ 7ｲ｀ﾄ　 ﾚ'ｧ-ﾄ､!ハ|　 |         ？？？？？？？   
+//        　　　　 !,/7 '0'　　 ´0iソ| 　 |　　　
+//        　　　　 |.从"　　_　　 ,,,, / |./ 　 |
+//        　　　　 ﾚ'| i＞.､,,__　_,.イ / 　.i 　|
+//        　　　　　 ﾚ'| | / k_７_/ﾚ'ヽ,　ﾊ.　|
+//        　　　　　　 | |/i 〈|/　 i　,.ﾍ |　i　|
+//        　　　　　　.|/ /　ｉ： 　 ﾍ!　　＼　|
+//        　　　 　 　 kヽ>､ﾊ 　 _,.ﾍ､ 　 /､!
+//        　　　　　　 !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+//        　　　　　　 ﾚ'ヽL__|___i,___,ンﾚ|ノ
+//        　　　　　 　　　ﾄ-,/　|___./
+//        　　　　　 　　　'ｰ'　　!_,.:
+
+/**
+ * @ProjectName: design
+ * @Package: com.baka.code
+ * @ClassName: MaximumDepthofBinaryTree
+ * @Description: 给定一个二叉树，找出其最大深度。
+                    二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+                    说明: 叶子节点是指没有子节点的节点。
+ * @Author: 唐朝
+ * @CreateDate: 2018/12/2 20:57
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2018/12/2 20:57
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class MaximumDepthofBinaryTree {
+
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int left = maxDepth(root.left) + 1;
+            int right = maxDepth(root.right) + 1;
+            return left>right?left:right;
+        }
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
