746e8d6d30363e857a5495c3c3f3d0a6e8ef5fbe
threedbag <389124628@qq.com>
2018-12-02 20:54:00 +0800

    合并两个有序数组

--- leecode/src/main/java/com/baka/code/MergeSortedArray.java
+++ leecode/src/main/java/com/baka/code/MergeSortedArray.java
@@ -33,9 +33,7 @@ import java.util.Arrays;
  */
 public class MergeSortedArray {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for (int i = 0; i < n; i++) {
-            nums1[m+i]=nums2[i];
-        }
+        System.arraycopy(nums2, 0, nums1, m, n);
         Arrays.sort(nums1);
 
     }
