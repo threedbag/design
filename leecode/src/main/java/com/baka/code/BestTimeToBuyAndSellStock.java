8fd4cedb4b09b8195b40778b011cc862a2e37db0
threedbag <389124628@qq.com>
2018-12-04 21:20:09 +0800

    买卖股票的最佳时机

---
+++ leecode/src/main/java/com/baka/code/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,63 @@
+package com.baka.code;
+//く__,.ヘヽ.　　　　/　,ー､ 〉
+//        　　　　　＼ ', !-─‐-i　/　/´
+//        　　　 　 ／｀ｰ'　　　 L/／｀ヽ､
+//        　　 　 /　 ／,　 /|　 ,　 ,　　　 ',     ？？？？？？？？
+//        　　　ｲ 　/ /-‐/　ｉ　L_ ﾊ ヽ!　 i       ？BUG哪来的呢？
+//        　　　 ﾚ ﾍ 7ｲ｀ﾄ　 ﾚ'ｧ-ﾄ､!ハ|　 |         ？？？？？？？   
+//        　　　　 !,/7 '0'　　 ´0iソ| 　 |　　　
+//        　　　　 |.从"　　_　　 ,,,, / |./ 　 |
+//        　　　　 ﾚ'| i＞.､,,__　_,.イ / 　.i 　|
+//        　　　　　 ﾚ'| | / k_７_/ﾚ'ヽ,　ﾊ.　|
+//        　　　　　　 | |/i 〈|/　 i　,.ﾍ |　i　|
+//        　　　　　　.|/ /　ｉ： 　 ﾍ!　　＼　|
+//        　　　 　 　 kヽ>､ﾊ 　 _,.ﾍ､ 　 /､!
+//        　　　　　　 !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+//        　　　　　　 ﾚ'ヽL__|___i,___,ンﾚ|ノ
+//        　　　　　 　　　ﾄ-,/　|___./
+//        　　　　　 　　　'ｰ'　　!_,.:
+
+/**
+ * @ProjectName: design
+ * @Package: com.baka.code
+ * @ClassName: BestTimeToBuyAndSellStock
+ * @Description: 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ * 注意你不能在买入股票前卖出股票。
+ * @Author: 唐朝
+ * @CreateDate: 2018/12/2 21:14
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2018/12/2 21:14
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class BestTimeToBuyAndSellStock {
+    //垃圾算法
+    public int maxProfit(int[] prices) {
+        int num = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                if (prices[j] - prices[i] >= num) {
+                    num = prices[j] - prices[i];
+                }
+            }
+        }
+        return num;
+    }
+
+    //标准
+    public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int profit = 0;
+        for (int i : prices) {
+            min = i < min ? i : min;
+            profit = (i - min) > profit ? i - min : profit;
+        }
+
+        return profit;
+    }
+}
