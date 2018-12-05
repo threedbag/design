4e9bbde3cf5edbcb7f253e650dc8536e079a3e8e
threedbag <389124628@qq.com>
2018-12-05 18:56:14 +0800

    只出现一次的数字

---
+++ leecode/src/main/java/com/baka/code/SingleNumber.java
@@ -0,0 +1,48 @@
+package com.baka.code;
+//く__,.ヘヽ.　　　　/　,ー､ 〉
+//        　　　　　＼ ', !-─‐-i　/　/´
+//        　　　 　 ／｀ｰ'　　　 L/／｀ヽ､
+//        　　 　 /　 ／,　 /|　 ,　 ,　　　 ',     ？？？？？？？？
+//        　　　ｲ 　/ /-‐/　ｉ　L_ ﾊ ヽ!　 i       ？BUG哪来的呢？
+//        　　　 ﾚ ﾍ 7ｲ｀ﾄ　 ﾚ'ｧ-ﾄ､!ハ|　 |         ？？？？？？？   
+//        　　　　 !,/7 '0'　　 ´0iソ| 　 |　　　
+//        　　　　 |.从"　　_　　 ,,,, / |./ 　 |
+//        　　　　 ﾚ'| i＞.､,,__　_,.イ / 　.i 　|
+//        　　　　　 ﾚ'| | / k_７_/ﾚ'ヽ,　ﾊ.　|
+//        　　　　　　 | |/i 〈|/　 i　,.ﾍ |　i　|
+//        　　　　　　.|/ /　ｉ： 　 ﾍ!　　＼　|
+//        　　　 　 　 kヽ>､ﾊ 　 _,.ﾍ､ 　 /､!
+//        　　　　　　 !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+//        　　　　　　 ﾚ'ヽL__|___i,___,ンﾚ|ノ
+//        　　　　　 　　　ﾄ-,/　|___./
+//        　　　　　 　　　'ｰ'　　!_,.:
+
+import java.util.Arrays;
+
+/**
+ * @ProjectName: design
+ * @Package: com.baka.code
+ * @ClassName: SingleNumber
+ * @Description: 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * @Author: 唐朝
+ * @CreateDate: 2018/12/5 18:31
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2018/12/5 18:31
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        if (nums.length==1||nums[0]!=nums[1]){
+            return nums[0];
+        }
+        for (int i = 0; i < nums.length-1; i++) {
+            if (nums[i]!=nums[i+1]&&nums[i]!=nums[i-1]){
+                return nums[i];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
