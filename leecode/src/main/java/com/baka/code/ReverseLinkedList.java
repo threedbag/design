09b0e57b599f3d684a2012580f9c512ff92d8cd7
threedbag <389124628@qq.com>
2018-12-12 20:08:40 +0800

    翻转链表

---
+++ leecode/src/main/java/com/baka/code/ReverseLinkedList.java
@@ -0,0 +1,66 @@
+package com.baka.code;
+//く__,.ヘヽ.　　　　/　,ー､ 〉
+//        　　　　　＼ ', !-─‐-i　/　/´
+//        　　　 　 ／｀ｰ'　　　 L/／｀ヽ､
+//        　　 　 /　 ／,　 /|　 ,　 ,　　　 ',     ？？？？？？？？
+//        　　　ｲ 　/ /-‐/　ｉ　L_ ﾊ ヽ!　 i       ？BUG哪来的呢？
+//        　　　 ﾚ ﾍ 7ｲ｀ﾄ　 ﾚ'ｧ-ﾄ､!ハ|　 |         ？？？？？？？   
+//        　　　　 !,/7 '0'　　 ´0iソ| 　 |　　　
+//        　　　　 |.从"　　_　　 ,,,, / |./ 　 |
+//        　　　　 ﾚ'| i＞.､,,__　_,.イ / 　.i 　|
+//        　　　　　 ﾚ'| | / k_７_/ﾚ'ヽ,　ﾊ.　|
+//        　　　　　　 | |/i 〈|/　 i　,.ﾍ |　i　|
+//        　　　　　　.|/ /　ｉ： 　 ﾍ!　　＼　|
+//        　　　 　 　 kヽ>､ﾊ 　 _,.ﾍ､ 　 /､!
+//        　　　　　　 !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+//        　　　　　　 ﾚ'ヽL__|___i,___,ンﾚ|ノ
+//        　　　　　 　　　ﾄ-,/　|___./
+//        　　　　　 　　　'ｰ'　　!_,.:
+
+import java.util.ArrayList;
+
+/**
+ * @ProjectName: design
+ * @Package: com.baka.code
+ * @ClassName: ReverseLinkedList
+ * @Description: 反转一个单链表。
+ * @Author: 唐朝
+ * @CreateDate: 2018/12/12 17:01
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2018/12/12 17:01
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class ReverseLinkedList {
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode pre = head;
+        ListNode next = head.next;
+        ListNode now;
+        while (next != null) {
+            //取得第三个节点
+            now = next.next;
+            //将第二个节点的下一个指向第一个节点
+            next.next = pre;
+            //将2个指针都往下移一格
+            pre = next;
+            next = now;
+        }
+        head.next = null;
+        return pre;
+    }
+
+
+}
