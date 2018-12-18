4327ddd41dc2aa49af45cc6b85b0924cd769449c
threedbag <389124628@qq.com>
2018-12-18 11:40:26 +0800

    Nim游戏

---
+++ leecode/src/main/java/com/baka/code/NimGame.java
@@ -0,0 +1,45 @@
+package com.baka.code;
+//く__,.ヘヽ.　　　　/　,ー､ 〉
+//        　　　　　＼ ', !-─‐-i　/　/´
+//        　　　 　 ／｀ｰ'　　　 L/／｀ヽ､
+//        　　 　 /　 ／,　 /|　 ,　 ,　　　 ',     ？？？？？？？？
+//        　　　ｲ 　/ /-‐/　ｉ　L_ ﾊ ヽ!　 i       ？BUG哪来的呢？
+//        　　　 ﾚ ﾍ 7ｲ｀ﾄ　 ﾚ'ｧ-ﾄ､!ハ|　 |         ？？？？？？？   
+//        　　　　 !,/7 '0'　　 ´0iソ| 　 |　　　
+//        　　　　 |.从"　　_　　 ,,,, / |./ 　 |
+//        　　　　 ﾚ'| i＞.､,,__　_,.イ / 　.i 　|
+//        　　　　　 ﾚ'| | / k_７_/ﾚ'ヽ,　ﾊ.　|
+//        　　　　　　 | |/i 〈|/　 i　,.ﾍ |　i　|
+//        　　　　　　.|/ /　ｉ： 　 ﾍ!　　＼　|
+//        　　　 　 　 kヽ>､ﾊ 　 _,.ﾍ､ 　 /､!
+//        　　　　　　 !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+//        　　　　　　 ﾚ'ヽL__|___i,___,ンﾚ|ノ
+//        　　　　　 　　　ﾄ-,/　|___./
+//        　　　　　 　　　'ｰ'　　!_,.:
+
+/**
+ * @ProjectName: design
+ * @Package: com.baka.code
+ * @ClassName: NimGame
+ * @Description: 桌子上有一堆石头，每次你们轮流拿掉 1 - 3 块石头。 拿掉最后一块石头的人就是获胜者。你作为先手。
+ * @Author: 唐朝
+ * @CreateDate: 2018/12/18 11:36
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2018/12/18 11:36
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class NimGame {
+    /**
+     * 4的倍数情况下，后手必赢
+     * @param n
+     * @return
+     */
+    public boolean canWinNim(int n) {
+        if (n % 4 ==0) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+}
