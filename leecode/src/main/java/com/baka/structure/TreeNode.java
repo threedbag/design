01f7d32d35ec6c82b6828a2d112282889bc6d741
threedbag <389124628@qq.com>
2019-01-15 19:19:55 +0800

    弄个二叉树的类

---
+++ leecode/src/main/java/com/baka/structure/TreeNode.java
@@ -0,0 +1,40 @@
+package com.baka.structure;
+//く__,.ヘヽ.　　　　/　,ー､ 〉
+//        　　　　　＼ ', !-─‐-i　/　/´
+//        　　　 　 ／｀ｰ'　　　 L/／｀ヽ､
+//        　　 　 /　 ／,　 /|　 ,　 ,　　　 ',     ？？？？？？？？
+//        　　　ｲ 　/ /-‐/　ｉ　L_ ﾊ ヽ!　 i       ？BUG哪来的呢？
+//        　　　 ﾚ ﾍ 7ｲ｀ﾄ　 ﾚ'ｧ-ﾄ､!ハ|　 |         ？？？？？？？   
+//        　　　　 !,/7 '0'　　 ´0iソ| 　 |　　　
+//        　　　　 |.从"　　_　　 ,,,, / |./ 　 |
+//        　　　　 ﾚ'| i＞.､,,__　_,.イ / 　.i 　|
+//        　　　　　 ﾚ'| | / k_７_/ﾚ'ヽ,　ﾊ.　|
+//        　　　　　　 | |/i 〈|/　 i　,.ﾍ |　i　|
+//        　　　　　　.|/ /　ｉ： 　 ﾍ!　　＼　|
+//        　　　 　 　 kヽ>､ﾊ 　 _,.ﾍ､ 　 /､!
+//        　　　　　　 !'〈//｀Ｔ´', ＼ ｀'7'ｰr'
+//        　　　　　　 ﾚ'ヽL__|___i,___,ンﾚ|ノ
+//        　　　　　 　　　ﾄ-,/　|___./
+//        　　　　　 　　　'ｰ'　　!_,.:
+
+/**
+ * @ProjectName: design
+ * @Package: com.baka.structure
+ * @ClassName: TreeNode
+ * @Description: 二叉树
+ * @Author: 唐朝
+ * @CreateDate: 2019/1/15 19:18
+ * @UpdateUser: 更新者
+ * @UpdateDate: 2019/1/15 19:18
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
